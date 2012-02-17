41d298021244f6d7e8eda7b7b92c40fb783aeb3b
Shai Rosenfeld & Michael Brodhead <srosenfeld@engineyard.com>
2012-02-17 13:36:35 -0800

    Use rspec as default rake task.

--- Gemfile
+++ Gemfile
@@ -2,3 +2,4 @@ source "http://rubygems.org"
 gemspec
 gem 'rspec'
 gem 'timecop'
+gem 'rake'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,7 @@
 require 'rubygems'
 require 'bundler/setup'
 require 'timecop'
+require 'rspec'
 
 require 'rotp'
 
