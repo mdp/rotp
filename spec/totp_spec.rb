a11b94cc78a626ff1abe5b8dbd30e8bbcb5deec7
David Vrensk <david@vrensk.com>
2012-05-21 23:37:14 +0200

    Bug fix: TOTP#verify_with_drift produced wrong result when drift was not a multiple of interval

--- spec/totp_spec.rb
+++ spec/totp_spec.rb
@@ -26,10 +26,18 @@ describe ROTP::TOTP do
       subject.verify_with_drift(subject.at(@now - 30), 60, @now).should be_true
     end
     it "should verify a slightly new number" do
-      subject.verify_with_drift(subject.at(@now - 60), 60, @now).should be_true
+      subject.verify_with_drift(subject.at(@now + 60), 60, @now).should be_true
     end
     it "should reject a number that is outside the allowed drift" do
       subject.verify_with_drift(subject.at(@now - 60), 30, @now).should be_false
+    end
+    context "with drift that is not a multiple of the TOTP interval" do
+      it "should verify a slightly old number" do
+        subject.verify_with_drift(subject.at(@now - 45), 45, @now).should be_true
+      end
+      it "should verify a slightly new number" do
+        subject.verify_with_drift(subject.at(@now + 40), 40, @now).should be_true
+      end
     end
   end
 end
