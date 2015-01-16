316391a1cf5ba56d4693866fd445579b0405203b
Mark Percival <m@mdp.im>
2015-01-16 19:26:48 +0000

    More specific error, link to RFC wikipedia

--- lib/rotp/cli.rb
+++ lib/rotp/cli.rb
@@ -18,7 +18,7 @@ module ROTP
         if options.secret.to_s == ''
           red 'You must also specify a --secret. Try --help for help.'
         elsif options.secret.to_s.chars.any? { |c| ROTP::Base32::CHARS.index(c) == nil }
-          red 'Secret must be in Base32 format'
+          red 'Secret must be in RFC4648 Base32 format - http://en.wikipedia.org/wiki/Base32#RFC_4648_Base32_alphabet'
         end
       elsif options.mode == :hmac && options.counter.to_i < 0
         red 'You must also specify a --counter. Try --help for help.'
